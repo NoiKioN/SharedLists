a66b8fa0ca5076ad85b06113e5108e10e45de706
Knight NoiKioN <nnightknight@gmail.com>
2018-02-26 23:10:16 +0100

    DatabaseReader() updated, started moving tested code from TestActivity().

--- mobile/src/main/java/com/nnightknights/sharedlists/list/database/DatabaseReader.java
+++ mobile/src/main/java/com/nnightknights/sharedlists/list/database/DatabaseReader.java
@@ -1,11 +1,14 @@
 package com.nnightknights.sharedlists.list.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.widget.ProgressBar;
 
 import com.nnightknights.sharedlists.list.interfaces.List;
+
+import java.util.ArrayList;
 
 public class DatabaseReader extends AsyncTask<String, Integer, List> {
     DatabaseManager databaseManager;
@@ -40,8 +43,21 @@ public class DatabaseReader extends AsyncTask<String, Integer, List> {
 
     @Override
     protected List doInBackground(String... strings) {
+        Cursor cursor = listsDatabase.query(ListsContract.Lists.TABLE_NAME,
+                new String[]{ListsContract.Lists.COLUMN_NAME_TITLE},
+                ListsContract.Lists._ID + " = ?",
+                new String[]{"1"},
+                null,
+                null,
+                null);
+        java.util.List names = new ArrayList<String>();
+        while(cursor.moveToNext()){
+            names.add(cursor.getString(
+                    cursor.getColumnIndexOrThrow(ListsContract.Lists.COLUMN_NAME_TITLE)
+            ));
+        }
+        cursor.close();
         return null;
-
     }
 
     @Override
